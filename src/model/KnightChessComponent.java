06439e8e667f423947fb6835dcf016f0b16986e3
TimTerr <104203575+TimTerr@users.noreply.github.com>
2022-05-15 10:59:30 +0800

    Update KnightChessComponent.java

--- src/model/KnightChessComponent.java
+++ src/model/KnightChessComponent.java
@@ -1,7 +1,7 @@
 package model;
 
-import controller.ClickController;
 import view.ChessboardPoint;
+import controller.ClickController;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -9,45 +9,19 @@ import java.io.File;
 import java.io.IOException;
 
 public class KnightChessComponent extends ChessComponent{
-    /**
-     * 黑骑士和白骑士的图片，static使得其可以被所有骑士对象共享
-     * <br>
-     * FIXME: 需要特别注意此处加载的图片是没有背景底色的！！！
-     */
     private static Image KNIGHT_WHITE;
     private static Image KNIGHT_BLACK;
-
-    /**
-     * 骑士棋子对象自身的图片，是上面两种中的一种
-     */
     private Image knightImage;
 
-    public KnightChessComponent(ChessboardPoint chessboardPoint, Point location, ChessColor chessColor, ClickController clickController, int size) {
-        super(chessboardPoint, location, chessColor, clickController, size);
-        initiateKnightImage(chessColor);
-    }
-
-    /**
-     * 读取加载骑士棋子的图片
-     *
-     * @throws IOException
-     */
     public void loadResource() throws IOException {
         if (KNIGHT_WHITE == null) {
-            KNIGHT_WHITE = ImageIO.read(new File("./images/knight-white.png"));
+            KNIGHT_WHITE = ImageIO.read(new File("./images/Knight-white.png"));
         }
 
         if (KNIGHT_BLACK == null) {
-            KNIGHT_BLACK = ImageIO.read(new File("./images/knight-black.png"));
+            KNIGHT_BLACK = ImageIO.read(new File("./images/Knight-black.png"));
         }
     }
-
-
-    /**
-     * 在构造棋子对象的时候，调用此方法以根据颜色确定knightImage的图片是哪一种
-     *
-     * @param color 棋子颜色
-     */
 
     private void initiateKnightImage(ChessColor color) {
         try {
@@ -62,49 +36,27 @@ public class KnightChessComponent extends ChessComponent{
         }
     }
 
-
-    /**
-     * 骑士棋子的移动规则
-     *
-     * @param chessComponents 棋盘
-     * @param destination     目标位置，如(0, 0), (0, 7)等等
-     * @return 骑士棋子移动的合法性
-     */
+    public KnightChessComponent(ChessboardPoint chessboardPoint, Point location, ChessColor color, ClickController listener, int size) {
+        super(chessboardPoint, location, color, listener, size);
+        initiateKnightImage(color);
+    }
 
     @Override
     public boolean canMoveTo(ChessComponent[][] chessComponents, ChessboardPoint destination) {
         ChessboardPoint source = getChessboardPoint();
-        if (source.getX() == destination.getX()) {
-            int row = source.getX();
-            for (int col = Math.min(source.getY(), destination.getY()) + 1;
-                 col < Math.max(source.getY(), destination.getY()); col++) {
-                if (!(chessComponents[row][col] instanceof EmptySlotComponent)) {
+        if((Math.abs(destination.getX()-source.getX()) == 1 && Math.abs(destination.getY()-source.getY()) == 2) ||
+                (Math.abs(destination.getX()-source.getX()) == 2 && Math.abs(destination.getY()-source.getY()) == 1)) {
+                if (!(chessComponents[destination.getX()][destination.getY()] instanceof EmptySlotComponent))
                     return false;
-                }
             }
-        } else if (source.getY() == destination.getY()) {
-            int col = source.getY();
-            for (int row = Math.min(source.getX(), destination.getX()) + 1;
-                 row < Math.max(source.getX(), destination.getX()); row++) {
-                if (!(chessComponents[row][col] instanceof EmptySlotComponent)) {
-                    return false;
-                }
-            }
-        } else { // Not on the same row or the same column.
-            return false;
-        }
+        else{return false;}
         return true;
     }
 
-    /**
-     * 注意这个方法，每当窗体受到了形状的变化，或者是通知要进行绘图的时候，就会调用这个方法进行画图。
-     *
-     * @param g 可以类比于画笔
-     */
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-//        g.drawImage(rookImage, 0, 0, getWidth() - 13, getHeight() - 20, this);
+//        g.drawImage(KnightImage, 0, 0, getWidth() - 13, getHeight() - 20, this);
         g.drawImage(knightImage, 0, 0, getWidth() , getHeight(), this);
         g.setColor(Color.BLACK);
         if (isSelected()) { // Highlights the model if selected.
@@ -112,4 +64,5 @@ public class KnightChessComponent extends ChessComponent{
             g.drawOval(0, 0, getWidth() , getHeight());
         }
     }
+    
 }
