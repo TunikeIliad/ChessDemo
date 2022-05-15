0248114457748c8c01750e29c0c123cb11e1787f
TimTerr <104203575+TimTerr@users.noreply.github.com>
2022-05-15 11:02:33 +0800

    Update PawnChessComponent.java

--- src/model/PawnChessComponent.java
+++ src/model/PawnChessComponent.java
@@ -1,53 +1,27 @@
 package model;
 
-import controller.ClickController;
 import view.ChessboardPoint;
+import controller.ClickController;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
-public class PawnChessComponent extends ChessComponent {
-    /**
-     * 黑兵和白兵的图片，static使得其可以被所有兵对象共享
-     * <br>
-     * FIXME: 需要特别注意此处加载的图片是没有背景底色的！！！
-     */
+public class PawnChessComponent extends ChessComponent{
     private static Image PAWN_WHITE;
     private static Image PAWN_BLACK;
-
-    /**
-     * 兵棋子对象自身的图片，是上面两种中的一种
-     */
     private Image pawnImage;
 
-    public PawnChessComponent(ChessboardPoint chessboardPoint, Point location, ChessColor chessColor, ClickController clickController, int size) {
-        super(chessboardPoint, location, chessColor, clickController, size);
-        initiatePawnImage(chessColor);
-    }
-
-    /**
-     * 读取加载兵棋子的图片
-     *
-     * @throws IOException
-     */
     public void loadResource() throws IOException {
         if (PAWN_WHITE == null) {
-            PAWN_WHITE = ImageIO.read(new File("./images/pawn-white.png"));
+            PAWN_WHITE = ImageIO.read(new File("./images/Pawn-white.png"));
         }
 
         if (PAWN_BLACK == null) {
-            PAWN_BLACK = ImageIO.read(new File("./images/pawn-black.png"));
+            PAWN_BLACK = ImageIO.read(new File("./images/Pawn-black.png"));
         }
     }
-
-
-    /**
-     * 在构造棋子对象的时候，调用此方法以根据颜色确定pawnImage的图片是哪一种
-     *
-     * @param color 棋子颜色
-     */
 
     private void initiatePawnImage(ChessColor color) {
         try {
@@ -62,49 +36,54 @@ public class PawnChessComponent extends ChessComponent {
         }
     }
 
-
-    /**
-     * 兵棋子的移动规则
-     *
-     * @param chessComponents 棋盘
-     * @param destination     目标位置，如(0, 0), (0, 7)等等
-     * @return 兵棋子移动的合法性
-     */
+    public PawnChessComponent(ChessboardPoint chessboardPoint, Point location, ChessColor color, ClickController listener, int size) {
+        super(chessboardPoint, location, color, listener, size);
+        initiatePawnImage(color);
+    }
 
     @Override
     public boolean canMoveTo(ChessComponent[][] chessComponents, ChessboardPoint destination) {
-        ChessboardPoint source = getChessboardPoint();
-        if (source.getX() == destination.getX()) {
-            int row = source.getX();
-            for (int col = Math.min(source.getY(), destination.getY()) + 1;
-                 col < Math.max(source.getY(), destination.getY()); col++) {
-                if (!(chessComponents[row][col] instanceof EmptySlotComponent)) {
+        ChessboardPoint source = getChessboardPoint();ChessColor color = getChessColor();
+        int x0 = source.getX(), x1 = destination.getX();
+        if((getChessColor() == ChessColor.BLACK && source.getX() == 1) || (getChessColor() == ChessColor.WHITE && source.getX() == 6)){
+            if(source.getY() == destination.getY() && Math.abs(destination.getX()-source.getX())==2){
+                if (!(chessComponents[destination.getX()][destination.getY()] instanceof EmptySlotComponent))
                     return false;
+            }
+        }
+        if(source.getY() == destination.getY()){
+            if((color == ChessColor.BLACK && x0+1 == x1) || (color == ChessColor.WHITE && x0-1 == x1)){
+                if (!(chessComponents[x1][destination.getY()] instanceof EmptySlotComponent))
+                    return false;
+            }
+            if((color == ChessColor.BLACK && x0 == 1 && x0+2 == x1) || (color == ChessColor.WHITE && x0 == 6 && x0-2 == x1)){
+
+            }
+        }
+        if(chessColor == ChessColor.BLACK){
+            if(source.getY() == destination.getY()){
+                if(x0 == 1 && x0+2 == x1){
+                    while(x0 < x1){
+                        if (!(chessComponents[x0+1][destination.getY()] instanceof EmptySlotComponent)){
+                            return false;
+                        }
+                        else x0++;
+                    }
+                }
+                else if(x0+1 == x1){
+                    if (!(chessComponents[x0+1][destination.getY()] instanceof EmptySlotComponent))
+                        return false;
                 }
             }
-        } else if (source.getY() == destination.getY()) {
-            int col = source.getY();
-            for (int row = Math.min(source.getX(), destination.getX()) + 1;
-                 row < Math.max(source.getX(), destination.getX()); row++) {
-                if (!(chessComponents[row][col] instanceof EmptySlotComponent)) {
-                    return false;
-                }
-            }
-        } else { // Not on the same row or the same column.
-            return false;
+
         }
         return true;
     }
 
-    /**
-     * 注意这个方法，每当窗体受到了形状的变化，或者是通知要进行绘图的时候，就会调用这个方法进行画图。
-     *
-     * @param g 可以类比于画笔
-     */
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-//        g.drawImage(rookImage, 0, 0, getWidth() - 13, getHeight() - 20, this);
+//        g.drawImage(PawnImage, 0, 0, getWidth() - 13, getHeight() - 20, this);
         g.drawImage(pawnImage, 0, 0, getWidth() , getHeight(), this);
         g.setColor(Color.BLACK);
         if (isSelected()) { // Highlights the model if selected.
