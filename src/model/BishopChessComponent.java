f4fc7546fffbb829950b16ae6314c4ccdff41531
TimTerr <104203575+TimTerr@users.noreply.github.com>
2022-05-15 10:57:28 +0800

    Bishop Change

--- src/model/BishopChessComponent.java
+++ src/model/BishopChessComponent.java
@@ -1,53 +1,27 @@
 package model;
 
-import controller.ClickController;
 import view.ChessboardPoint;
+import controller.ClickController;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
-public class BishopChessComponent extends ChessComponent {
-    /**
-     * 黑教皇和白教皇的图片，static使得其可以被所有教皇对象共享
-     * <br>
-     * FIXME: 需要特别注意此处加载的图片是没有背景底色的！！！
-     */
+public class BishopChessComponent extends ChessComponent{
     private static Image BISHOP_WHITE;
     private static Image BISHOP_BLACK;
-
-    /**
-     * 教皇棋子对象自身的图片，是上面两种中的一种
-     */
     private Image bishopImage;
 
-    public BishopChessComponent(ChessboardPoint chessboardPoint, Point location, ChessColor chessColor, ClickController clickController, int size) {
-        super(chessboardPoint, location, chessColor, clickController, size);
-        initiateBishopImage(chessColor);
-    }
-
-    /**
-     * 读取加载教皇棋子的图片
-     *
-     * @throws IOException
-     */
     public void loadResource() throws IOException {
         if (BISHOP_WHITE == null) {
-            BISHOP_WHITE = ImageIO.read(new File("./images/bishop-white.png"));
+            BISHOP_WHITE = ImageIO.read(new File("./images/Bishop-white.png"));
         }
 
         if (BISHOP_BLACK == null) {
-            BISHOP_BLACK = ImageIO.read(new File("./images/bishop-black.png"));
+            BISHOP_BLACK = ImageIO.read(new File("./images/Bishop-black.png"));
         }
     }
-
-
-    /**
-     * 在构造棋子对象的时候，调用此方法以根据颜色确定bishopImage的图片是哪一种
-     *
-     * @param color 棋子颜色
-     */
 
     private void initiateBishopImage(ChessColor color) {
         try {
@@ -62,49 +36,31 @@ public class BishopChessComponent extends ChessComponent {
         }
     }
 
-
-    /**
-     * 教皇棋子的移动规则
-     *
-     * @param chessComponents 棋盘
-     * @param destination     目标位置，如(0, 0), (0, 7)等等
-     * @return 教皇棋子移动的合法性
-     */
+    public BishopChessComponent(ChessboardPoint chessboardPoint, Point location, ChessColor color, ClickController listener, int size) {
+        super(chessboardPoint, location, color, listener, size);
+        initiateBishopImage(color);
+    }
 
     @Override
     public boolean canMoveTo(ChessComponent[][] chessComponents, ChessboardPoint destination) {
         ChessboardPoint source = getChessboardPoint();
-        if (source.getX() == destination.getX()) {
-            int row = source.getX();
-            for (int col = Math.min(source.getY(), destination.getY()) + 1;
-                 col < Math.max(source.getY(), destination.getY()); col++) {
-                if (!(chessComponents[row][col] instanceof EmptySlotComponent)) {
+        if(Math.abs(destination.getX()-source.getX()) == Math.abs(destination.getY()-source.getY())){
+            int row = source.getX(), col = source.getY(), x = (destination.getX()-source.getX() < 0)? -1 : 1,
+                    y = (destination.getY()-source.getY() < 0)? -1 : 1, counter = Math.abs(destination.getX()-source.getX());
+            while(counter-1 > 0){
+                row += x;col += y;
+                if (!(chessComponents[row][col] instanceof EmptySlotComponent))
                     return false;
-                }
+                counter--;
             }
-        } else if (source.getY() == destination.getY()) {
-            int col = source.getY();
-            for (int row = Math.min(source.getX(), destination.getX()) + 1;
-                 row < Math.max(source.getX(), destination.getX()); row++) {
-                if (!(chessComponents[row][col] instanceof EmptySlotComponent)) {
-                    return false;
-                }
-            }
-        } else { // Not on the same row or the same column.
-            return false;
-        }
+        }else{return false;}
         return true;
     }
 
-    /**
-     * 注意这个方法，每当窗体受到了形状的变化，或者是通知要进行绘图的时候，就会调用这个方法进行画图。
-     *
-     * @param g 可以类比于画笔
-     */
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-//        g.drawImage(rookImage, 0, 0, getWidth() - 13, getHeight() - 20, this);
+//        g.drawImage(BishopImage, 0, 0, getWidth() - 13, getHeight() - 20, this);
         g.drawImage(bishopImage, 0, 0, getWidth() , getHeight(), this);
         g.setColor(Color.BLACK);
         if (isSelected()) { // Highlights the model if selected.
