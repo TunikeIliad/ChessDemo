382fb1ed904b7bdee1ea9702fd249403721c1b60
TunikeIliad <104202561+TunikeIliad@users.noreply.github.com>
2022-05-15 10:55:26 +0800

    test3

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ import view.ChessGameFrame;
 import view.ChessboardStartScreen;
 
 import javax.swing.*;
-//test github
-//test 2
+
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
